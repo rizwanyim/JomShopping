33a9bb5b1f87e8ae1f98e653fced4487e763d0ee
rizwanyim <rizwanyim@gmail.com>
2026-01-19 00:40:38 +0800

    Update vite.config.ts

--- jomshopping/vite.config.ts
+++ jomshopping/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Kita naikkan limit warning ni ke 1600kb (1.6MB) supaya dia diam
+    chunkSizeWarningLimit: 1600,
+  },
 })
