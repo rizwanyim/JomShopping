f94e485b76428fcf676809671bf2bb24783a5b40
rizwanyim <rizwanyim@gmail.com>
2026-01-19 00:49:31 +0800

    Add files via upload

--- jomshopping/vite.config.ts
+++ jomshopping/vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // Kita naikkan limit warning ni ke 1600kb (1.6MB) supaya dia diam
-    chunkSizeWarningLimit: 1600,
-  },
 })
